caef970c5fff2378041c1437a2d0b14a310b7030
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 15:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var i = 0;
+  while(i < musicians.length) {
+    console.log(musicians[i] + " plays " + instruments[i]);
+  }
+}
