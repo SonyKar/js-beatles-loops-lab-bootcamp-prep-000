1fc3c5bf7c36f2d7ec21451a65dce35690f7e2eb
Alex Caraganciu <sa.caraganciu@gmail.com>
2018-09-05 15:56:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var i = 0;
   var strings = [];
   while(i < musicians.length) {
-    strings[i] = musicians[i] + " plays " + instruments[i];
+    strings[i].unshift(musicians[i] + " plays " + instruments[i]);
     i++;
   }
 }
